182f3a30a20433444d335e3380b6b8acd1d25fd1
DylanBoylan <a00283448@student.ait.ie>
2021-10-27 16:18:56 +0100

    Enemy (basic)

---
+++ SwingingGame/Assets/Code/Enemies/EnemyAI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public float walkSpeed;
+    public bool mustPatrol;
+    public bool mustTurn;
+
+    public Rigidbody2D rb;
+    public Transform GroundCheck;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPatrol)
+        {
+            Patrol();
+        }
+    }
+    private void FixedUpdate()
+    {
+       if (mustPatrol)
+        {
+            mustTurn = !Physics2D.OverlapCircle(GroundCheck.position, 0.0001f, groundLayer);
+        }
+    }
+    void Patrol()
+    {
+        if(mustTurn)
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        mustPatrol = false;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+}
