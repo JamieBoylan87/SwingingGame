7a412a26321c13627398f91bf179ab39f13bacef
DylanBoylan <a00283448@student.ait.ie>
2021-11-05 00:33:04 +0000

    Menus and death done

--- SwingingGame/Assets/Main Menu/MainMenu.cs
+++ SwingingGame/Assets/Main Menu/MainMenu.cs
@@ -5,14 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void Play ()
+    public void NextLevel ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Previous()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     public void Quit()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+
+    public void ToTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Start()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void Controls()
+    {
+        SceneManager.LoadScene("Controls");
     }
 }
